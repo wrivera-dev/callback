9f9767ce87a06f09e0b9a0763bec69dff4074e32
Will Rivera <willrivera@Wills-iMac.fios-router.home>
2018-01-25 10:18:06 -0500

    updates

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+As Browser Extension
 1. Unzip callbackExt folder
 2. Go to chrome://extensions/
 3. Click Load unpacked extension and select folder: callbackExt  (Ensure it is Enabked)
 4. Go to any URL to test
+
+---------- or -------------
+
+use content.js as a snippet
--- content.js
+++ content.js
@@ -10,9 +10,9 @@ function init() {
 }
 
 function callbackWhenReady(callback) {
-	if (document.readyState === "complete") {  
+	if (document.readyState === "complete") { 
         clearInterval(readyStateCheckInterval);
-        callback();  
+        callback(); 
 		loading=false;
 		interactive = false;
     }
@@ -37,24 +37,24 @@ var readyStateCheckInterval = setInterval(function() {
 	}
 }, 10);
 
-
-function document_ready_event() {  
+//   detect dynamic content loaded 
+function document_ready_event() { 
 	console.log("page ready");
 	
-	checkPageForTagInterval = setInterval(function() {	 
-		var element = document.querySelectorAll(".core-rail .main-container .container");	 
+	checkPageForTagInterval = setInterval(function() { 
+		var element = document.querySelectorAll(".core-rail .main-container .container");	// linkedin main feed class attribute .core-rail
 
 		if (element) {
-			clearInterval(checkPageForTagInterval);	 
-			document_complete_event();	 
+			clearInterval(checkPageForTagInterval);	
+			document_complete_event();
 		}
 	}, 10);
 }
- 
+
+// -------------------------------------------------------------
 function document_complete_event() {
 	for(var i = 0, elems = document.getElementsByTagName('h2'); i < elems.length; i++) { 
-   		elems[i].innerHTML = "<span style='font-size:40px; color:#1981E2;'>" + "Callback Extension: New HEADER 2" + "</span>";
- 
+   		elems[i].innerHTML = "<span style='font-size:20px; color:#1981E2;'>" + "New HEADER 2" + "</span>";
 	}
 	console.log("Tag Detected Document Complete!");
 	alert("Hi! Page loaded and Tag detected")
@@ -67,3 +67,16 @@ function document_loading_event() {
 function document_start_loading_event() {
 	console.log("start loading page");
 }
+
+
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "/";
+
+xhr.open(method, url, true);
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
+xhr.send();
